b7ed59beb30b0993341f6bf3f0e0d771b928361f
Aleksandrova Olga <bulbenko@yandex.ru>
2025-05-06 13:42:29 +0200

    fix: delete div.app from index.html

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import './style.scss';
 import App from './App.vue';
-
+const appContainer = document.createElement('div');
+appContainer.id = 'app';
+document.body.appendChild(appContainer);
 createApp(App).mount('#app');
