38d5c314edb2ea521f253a2e8fc0c2c14754c715
Aleksandrova Olga <bulbenko@yandex.ru>
2025-05-08 21:09:52 +0200

    feat: add validation form

--- src/components/layout/EmailInput.vue
+++ src/components/layout/EmailInput.vue
@@ -9,15 +9,14 @@ const errorEmail = ref<string>('');
 function validateEmail(event: Event) {
   const value = event.target.value.trim();
   emit('update:modelValue', value);
+  
   const errorMessage = 'Email address must contain an "@" symbol, local part and domain name.';
   errorEmail.value = isEmail(value) ? '' : errorMessage;
  
 }
 function isEmail(email: string): boolean {
-  if (!email.includes('@')) return false;
-  const [localPart, domain] = email.split('@');
-  if (localPart === '' || domain === '') return false;
-  return true;
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
 }
 </script>
 <template>
