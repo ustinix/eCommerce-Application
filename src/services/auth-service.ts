b19d7c229c7fe2d0a5231e9d47ac6091360a3394
Aleksandrova Olga <bulbenko@yandex.ru>
2025-05-13 17:32:40 +0200

    feat: getting a token

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,17 +1,18 @@
 import {
-  createAuthForClientCredentialsFlow,
+  /*createAuthForClientCredentialsFlow,*/
   createHttpClient,
   createClient,
 } from '@commercetools/sdk-client-v2';
+import { createAuthForPasswordFlow } from '@commercetools/sdk-client-v2';
 
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 
 import { type ApiError } from '../types/api-error';
-import { getAuthToken } from './get-token';
+//import { getAuthToken } from './get-token';
 
 const projectKey = 'rss-ecom';
 
-const client = createClient({
+/*const client = createClient({
   middlewares: [
     createAuthForClientCredentialsFlow({
       host: 'https://auth.us-central1.gcp.commercetools.com',
@@ -29,6 +30,8 @@ const client = createClient({
 });
 
 export const apiRoot = createApiBuilderFromCtpClient(client).withProjectKey({ projectKey });
+*/
+
 
 export const loginCustomer = async (
   email: string,
@@ -37,7 +40,28 @@ export const loginCustomer = async (
   loginFailed: (error: string) => void,
 ): Promise<void> => {
   try {
-    const response = await apiRoot
+    const passwordClient = createClient({
+      middlewares: [
+        createAuthForPasswordFlow({
+          host: 'https://auth.us-central1.gcp.commercetools.com',
+          projectKey,
+          credentials: {
+            clientId: import.meta.env.VITE_CTP_CLIENT_ID,
+            clientSecret: import.meta.env.VITE_CTP_CLIENT_SECRET,
+            user: {
+              username: email,
+              password,
+            },
+          },
+        }),
+        createHttpClient({
+          host: 'https://api.us-central1.gcp.commercetools.com',
+          fetch: globalThis.fetch,
+        }),
+      ],
+    });
+  const apiRoot = createApiBuilderFromCtpClient(passwordClient).withProjectKey({ projectKey });
+    /*  const response = await apiRoot
       .login()
       .post({
         body: {
@@ -51,8 +75,11 @@ export const loginCustomer = async (
 
     const result = await getAuthToken(email, password);
 
-    console.log('result', result);
-    loginValid();
+    console.log('result', result);*/
+    const response = await apiRoot.me().get().execute();
+    //loginValid();
+
+    console.log('response', response);
   } catch (error: unknown) {
     const defaultError = 'Server authentication error';
     const errorMessage = isCorrectError(error) ? error.message : defaultError;
