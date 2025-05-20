7007a1c35170fadda4378ae1cd07208780b900f5
Ustinix <ksuletters@yandex.ru>
2025-05-20 14:55:48 +0300

    refactor: fix remarks after PR

--- src/utils/token-decoder.js
+++
@@ -1,6 +0,0 @@
-export const encodeToken = (token) => {
-    return btoa(encodeURIComponent(token));
-};
-export const decodeToken = (encoded) => {
-    return decodeURIComponent(atob(encoded));
-};
