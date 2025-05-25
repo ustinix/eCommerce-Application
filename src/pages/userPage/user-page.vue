6d0ac008c272576dfebd2f826d170c81bf526f7f
Aleksandrova Olga <bulbenko@yandex.ru>
2025-05-25 15:51:39 +0200

    feat: add user page view

--- src/pages/userPage/user-page.vue
+++ src/pages/userPage/user-page.vue
@@ -1,26 +1,59 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useAuthStore } from '../../stores/auth';
 import router from '../../router/router';
 import { getUserData } from '../../services/user-service';
+import type { Customer } from '@commercetools/platform-sdk';
+import UserView from '../../components/layout/user-view.vue';
 
-const title = 'User profile';
+const enum textPage {
+  title = 'User profile',
+  sectionPersonal = 'Personal information',
+  sectionAddresses = 'Addresses',
+  defaultShipping = 'Default Shipping Address',
+  defaultBilling = 'Default Billing Address',
+  editButton = 'Edit',
+  saveButton = 'Save',
+  cancelButton = 'Cancel',
+  errorLoading = 'Error loading user data',
+}
 
 const authStore = useAuthStore();
+let isEditing = ref(false);
+let profile = ref<Customer | null>(null);
+let errorPage = ref<string | null>(null);
 
 if (!authStore.isAuthenticated) {
   router.push('/');
 }
 onMounted(async () => {
-  await getUserData(authStore);
+  try {
+    profile.value = await getUserData(authStore);
+    errorPage.value = profile.value ? null : textPage.errorLoading;
+  } catch {
+    errorPage.value = textPage.errorLoading;
+  }
 });
+const toggleEdit = (): void => {
+  isEditing.value = !isEditing.value;
+};
 </script>
 
 <template>
-  <div class="profile">
-    <h2 class="profile_title">{{ title }}</h2>
+  <div class="hero-user">
+    <h2 class="hero_title">{{ textPage.title }}</h2>
+  </div>
+  <p v-if="errorPage" class="error_text">{{ errorPage }}</p>
+  <div class="profile" v-if="profile">
+    <div v-if="!isEditing">
+      <UserView :profile="profile" v-if="!isEditing" />
+      <button class="button" @click="toggleEdit">{{ textPage.editButton }}</button>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-@use '../../assets/styles/variables.scss' as v;
+@use '../../assets/styles/hero.scss' as *;
+.hero-user {
+  @include hero-section('../../assets/images/profile.png');
+}
 </style>
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,28 +1,12 @@
 import type { Customer } from '@commercetools/platform-sdk';
 import type { useAuthStore } from '../stores/auth';
-// import type { ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
 
 export async function getUserData(
   authStore: ReturnType<typeof useAuthStore>,
-): Promise<Customer | undefined> {
-  if (!authStore.currentApiRoot) return;
+): Promise<Customer | null> {
+  if (!authStore.currentApiRoot) return null;
   const response = await authStore.currentApiRoot.me().get().execute();
   console.log(response);
 
   return response.body;
 }
-/*
-export async function fetchAllProducts(): Promise<ProductProjectionPagedQueryResponse | null> {
-  if (!authStore.currentApiRoot) return null;
-  const response = await authStore.currentApiRoot
-    .productProjections()
-    .get({
-      queryArgs: {
-        limit: 100,
-      },
-    })
-    .execute();
-  console.log('product', response.body);
-  return response.body;
-}
-*/
