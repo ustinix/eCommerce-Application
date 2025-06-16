83fced49a620641b6e84e5aed48339677915c99f
Ustinix <ksuletters@yandex.ru>
2025-06-16 10:06:17 +0300

    feat: add test formatprice

--- src/utils/format-price.ts
+++ src/utils/format-price.ts
@@ -1,8 +1,8 @@
 import { dollarSing } from '../constants/constants';
 
 export const formatPrice = (price: number | undefined): string => {
-  if (!price) {
-    return '';
+  if (price !== undefined && price > 0) {
+    return `${dollarSing}${(price / 100).toFixed(2)}`;
   }
-  return `${dollarSing}${(price / 100).toFixed(2)}`;
+  return '';
 };
